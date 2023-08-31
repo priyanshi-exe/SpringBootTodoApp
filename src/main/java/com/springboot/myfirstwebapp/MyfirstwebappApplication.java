7e378d8db1b76b49f351c0339394c653673498a4
Priyanshi Agarwal <priyanshiagarwal@Priyanshis-MacBook-Pro.local>
2023-08-31 17:31:05 +0530

    First commit

---
+++ src/main/java/com/springboot/myfirstwebapp/MyfirstwebappApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.myfirstwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/myfirstwebapp/MyfirstwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.myfirstwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
